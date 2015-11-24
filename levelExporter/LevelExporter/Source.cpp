debff865a56e7077b4f76979f013910ca2fccbbf
julietbox <nekodoru@hotmail.com>
2015-11-24 16:25:30 +0100

    Level exporter currently looks through meshes in scene and finds the extra attributes. Doesn't export anything yet.

---
+++ levelExporter/LevelExporter/Source.cpp
@@ -0,0 +1,211 @@
+#include "maya_includes.h"
+#include "Structs.h"
+#include <iostream>
+#include <fstream>
+#include <DirectXMath.h>
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+
+using namespace DirectX;
+using namespace std;
+
+//UI
+void initUI();
+void deleteUI();
+
+//export
+void exportToFile();
+
+class exportLevel : public MPxCommand
+{
+public:
+	exportLevel() {};
+	~exportLevel() {};
+
+	virtual MStatus doIt(const MArgList&)
+	{
+		setResult("Export Level Called\n");
+		MGlobal::displayInfo("Button press!");
+		exportToFile();
+		return MS::kSuccess;
+	}
+
+	static void* creator()
+	{
+		return new exportLevel;
+	}
+
+};
+
+
+
+// called when the plugin is loaded
+EXPORT MStatus initializePlugin(MObject obj)
+{
+	// most functions will use this variable to indicate for errors
+	MStatus res = MS::kSuccess;
+
+
+	MFnPlugin plugin(obj, "LevelExporter", "1.0", "Any", &res);
+	if (MFAIL(res)) {
+		CHECK_MSTATUS(res);
+	}
+
+	MStatus status;
+	// register our commands with maya
+	status = plugin.registerCommand("exportLevel", exportLevel::creator);
+	
+
+	initUI();
+
+
+	MGlobal::displayInfo("Maya plugin loaded!!");
+
+
+	return res;
+}
+
+
+EXPORT MStatus uninitializePlugin(MObject obj)
+{
+	// simply initialize the Function set with the MObject that represents
+	// our plugin
+	MFnPlugin plugin(obj);
+
+	// if any resources have been allocated, release and free here before
+	// returning...
+
+	MStatus status;
+	// deregister our commands with maya
+	status = plugin.deregisterCommand("exportLevel");
+
+
+	deleteUI();
+	
+	MGlobal::displayInfo("Maya plugin unloaded!!");
+
+	return MS::kSuccess;
+}
+
+
+void initUI()
+{
+	MGlobal::executeCommand("window -wh 250 105 -s true -title ""LevelExporter"" ""levelExporterUI"";");
+	MGlobal::executeCommand("columnLayout -columnAttach ""left"" 5 -rowSpacing 5 -columnWidth 100;;");
+	MGlobal::executeCommand("button -w 200 -h 50 -label ""Export_Level"" -command ""exportLevel"";");
+	
+	
+	
+	MGlobal::executeCommand("showWindow;");
+	
+	MGlobal::displayInfo("UI created");
+}
+
+void deleteUI()
+{
+	MGlobal::executeCommand("deleteUI -window ""levelExporterUI"";");
+	MGlobal::displayInfo("UI deleted");
+}
+
+void exportToFile()
+{
+
+	MGlobal::displayInfo("Test of button and things");
+
+	MItDag itMeshes(MItDag::kDepthFirst, MFn::kMesh);
+	
+	while (!itMeshes.isDone())
+	{
+		MFnMesh meshTemp = itMeshes.currentItem();
+		MFnTransform transformTemp = meshTemp.parent(0);
+		
+		//
+		//MObject test = transformTemp.attribute("tileType");
+		//MFn::kEnumAttribute;
+		//MFnEnumAttribute enumTest = test;
+		//MString attributeName = enumTest.name();
+
+		//MGlobal::displayInfo(attributeName);
+
+		MGlobal::displayInfo(MString() + meshTemp.name());
+
+		if (transformTemp.hasAttribute("tileType"))
+		{
+			//find type of tile
+			MFnEnumAttribute tileEnum = transformTemp.attribute("tileType");
+			MGlobal::displayInfo("Has tiletype attr");
+
+			MString tileType = tileEnum.fieldName(0);
+
+			if (strcmp(tileType.asChar(), "floorTile"))
+			{
+				MGlobal::displayInfo("tile says " + tileType);
+			}
+
+
+		}
+		if (transformTemp.hasAttribute("walkable"))
+		{
+			MFnEnumAttribute walkEnum = transformTemp.attribute("walkable");
+			MGlobal::displayInfo("has walkable attr");
+
+
+			MString answer = walkEnum.fieldName(0);
+
+			if (strcmp(answer.asChar(), "Yes"))
+			{
+				MGlobal::displayInfo("walkable: yes");
+			}
+			if (strcmp(answer.asChar(), "No"))
+			{
+				MGlobal::displayInfo("walkable: no");
+			}
+
+
+		}
+		if (transformTemp.hasAttribute("entrance"))
+		{
+			MFnEnumAttribute entranceEnum = transformTemp.attribute("entrance");
+			MGlobal::displayInfo("has entrance attr");
+
+			MString answer = entranceEnum.fieldName(0);
+
+
+			if (strcmp(answer.asChar(), "Yes"))
+			{
+				MGlobal::displayInfo("entrance: yes");
+			}
+			if (strcmp(answer.asChar(), "No"))
+			{
+				MGlobal::displayInfo("entrance: no");
+			}
+
+		}
+		if (transformTemp.hasAttribute("goal"))
+		{
+			MFnEnumAttribute goalEnum = transformTemp.attribute("goal");
+			MGlobal::displayInfo("has goal attr");
+			MString answer = goalEnum.fieldName(0);
+
+
+			if (strcmp(answer.asChar(), "Yes"))
+			{
+				MGlobal::displayInfo("goal: yes");
+			}
+			if (strcmp(answer.asChar(), "No"))
+			{
+				MGlobal::displayInfo("goal: no");
+			}
+		}
+
+		
+		
+		
+
+		itMeshes.next();
+	}
+}
+
---
+++ levelExporter/LevelExporter/Structs.h
@@ -0,0 +1,94 @@
+#pragma once
+#include <vector>
+
+struct Point
+{
+	float x, y, z;
+	int boneIndices[4];
+	float boneWeigths[4];
+};
+
+struct Normal
+{
+	float x, y, z;
+};
+
+
+struct TexCoord
+{
+	float u, v;
+};
+
+struct Vertex
+{
+	int pointID, normalID, texCoordID;
+};
+
+struct Face
+{
+	Vertex verts[3];
+};
+
+struct VertexOut
+{
+	float pos[3], nor[3], uv[2];
+};
+
+struct Material
+{
+	float diffColor[4], specColor[4];
+	std::string diffuseTexture, specularTexture;
+};
+
+struct PointLight
+{
+	float pos[3], col[3], intensity;
+};
+
+struct SpotLight
+{
+	float pos[3], col[3], intensity, angle, direction[3];
+};
+
+struct Geometry
+{
+	std::vector<Point> points;
+	std::vector<Normal> normals;
+	std::vector<TexCoord> texCoords;
+	std::vector<Face> faces;
+	std::vector<VertexOut> vertices;
+	
+	std::vector<PointLight> pointLights;
+	std::vector<SpotLight> spotLights;
+};
+
+struct SubMesh
+{
+	std::string Name;
+	Geometry geometry;
+};
+
+struct Mesh
+{
+	std::string Name;
+	int skeletonID;
+	std::vector<SubMesh> subMeshes;
+	Geometry geometry;
+	Material material;
+};
+
+//Headers-------------
+
+
+struct MainHeader {
+	int version, meshCount;
+};
+
+struct MeshHeader
+{
+	int nameLength, numberOfVertices, subMeshID, numberPointLights, numberSpotLights;
+};
+
+struct MatHeader {
+	int diffuseNameLength, specularNameLength;
+};
